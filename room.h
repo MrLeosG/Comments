11b0acc3d9501b9b7b98126a36b82f411d7dfa19
ijcoffman <ijcoffman@cpp.edu>
2026-03-04 20:03:42 -0800

    Copied room file

--- room.h
+++ room.h
@@ -1,12 +1,34 @@
-#ifndef ROOM_H
-#define ROOM_H
+#include <iostream>
+#include <random>
+#include "interactions.h"
 
-class Room {
-public:
-    Room(const std::string& description);
-    std::string getDescription() const;
-private:
-    std::string roomDescription;
-};
+int get_random_foe() {
+    static std::random_device rd;  
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(1, 3);
 
-#endif // ROOM_H
+    return dist(gen);
+}
+
+void room_one() {
+    int Wait =0;
+    int foe = get_random_foe();
+    std::string foeName = "";
+    if (foe == 1) {
+        foeName = "Skeleton";
+    } else if (foe == 2) {
+        foeName = "Troll";
+    } else if (foe == 3) {
+        foeName = "Swarm of Cicadas";
+    }
+    std::cout << "A Dangerous foe appears in the first room of the Admissions Building" << std::endl;
+    std::cout << "The foe is a:" << foeName << std::endl;
+    std::cout << "What will you do? (1: Fight/n 2: Check Inventory/n 3: Check HP): ";
+    std::cin >> action;
+    if (action ==1) {
+        damage_interaction();
+    }
+
+    std::cin >> Wait;
+
+}
